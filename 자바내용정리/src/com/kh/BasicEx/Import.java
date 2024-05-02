5a3045345949a67d3e715fdd6e0fababe6f15b73
kyungnankimKHT <165994108+kyungnankimKHT@users.noreply.github.com>
2024-05-02 15:39:36 +0900

    java

--- 자바내용정리/src/com/kh/BasicEx/Import.java
+++ 자바내용정리/src/com/kh/BasicEx/Import.java
@@ -6,14 +6,19 @@ import
 	사용예제 
 		import 패키지명.클래스명;
 		
-		import java.Util.ArrayList; 
-		// java 폴더 밑에 Util 폴더 안에 있는 ArrayList 파일을 사용한다는 의미
 		
+		import java.Util.List; 
+		import java.Util.HashMap; 
+		import java.Util.ArrayList; 
+		java 폴더 밑에 Util 폴더 안에 있는 ArrayList 파일을 사용한다는 의미
+		똑같은 폴더 경로 위치라고 해도 가져오는 것을 선택해서 갖고 오길  원한다면
+		위처럼 가지고 와서 사용할 수 있음
+		    
 		import java.Util.*; 
-		// java 폴더 밑에 Util 폴더 안에 있는 모든 파일을 가져올 수 있음을 의미
-		//  * 를 쓴다하여 모든 파일을 가지고오게되면 프로그램 속도가 낮아질 수 있기 때문에
-		//  모두 가져오는 것이 아니라 가져올 수 있는 준비가 완료된 것임
-		 // 파일 위치 경로 설정
+		java 폴더 밑에 Util 폴더 안에 있는 모든 파일을 가져올 수 있음을 의미
+		* 를 쓴다하여 모든 파일을 가지고오게되면 프로그램 속도가 낮아질 수 있기 때문에
+		모두 가져오는 것이 아니라 가져올 수 있는 준비가 완료된 것임
+		파일 위치 경로 설정
  */
 public class Import {
 
