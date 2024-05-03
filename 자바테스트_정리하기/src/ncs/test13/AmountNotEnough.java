54aa995d33ecd7e60743f5786c0228139df74a47
kyungnankimKHT <165994108+kyungnankimKHT@users.noreply.github.com>
2024-05-03 12:49:23 +0900

    push

--- 자바테스트_정리하기/src/ncs/test13/AmountNotEnough.java
+++ 자바테스트_정리하기/src/ncs/test13/AmountNotEnough.java
@@ -1,5 +1,17 @@
 package ncs.test13;
-
-public class AmountNotEnough {
-
+//extends Exception
+public class AmountNotEnough extends Exception {
+	//양이 충분하지 않을 때 표시하는 에러를 개발자가 만들어준 것
+	public AmountNotEnough (String msg) {
+		//if new throws
+		/*
+		 	//만약에 제품이 존재하지 않을 경우  AmountNotEnough 넣고 사용을 할 것
+		for(Inventory 인벤토리 : inventoryMap.values()) {
+			if(인벤토리.getGetAmount() > 인벤토리.getPutAmount()) {
+				throw new AmountNotEnough("출고 수량이 재고 수량을 초과했습니다.");
+			}
+		} 
+		 */
+		super(msg);
+	}
 }
